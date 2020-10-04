6b3f91367bb2ff55e1a5231c918dca80aea91a70
赵玥 <44914129+FTITG@users.noreply.github.com>
2020-10-04 21:42:45 +0800

    Add files via upload

---
+++ precache-manifest.a3535bf6ab8aed011197d7b257730dcd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5048be98fae76434d9433d63eb21e229",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc32e9450d867891c176",
+    "url": "/static/js/2.4a4c7e4d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4a4c7e4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf22474fcf77b179ef0b",
+    "url": "/static/js/main.4fe2734a.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
